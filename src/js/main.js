caa820729f2197943f4e9927212850e13119979c
justin <justin2018777@gmail.com>
2018-05-11 17:13:25 +0800

    Add background color modification while mouse over the key of caculator.

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+function onSignKeyMouseOver(element) {
+  element.style.backgroundColor = "#ffe4e1"; // mistyrose
+}
+
+function onSignKeyMouseOut(element) {
+  element.style.backgroundColor = "#ffa07a"; // lightsalmon
+}
+
+function onNumberKeyMouseOver(element) {
+  element.style.backgroundColor = "#f5f5dc"; // beige
+}
+
+function onNumberKeyMouseOut(element) {
+  element.style.backgroundColor = "#f0e68c"; // khaki
+}
+
+function onResultFieldMouseOver(element) {
+  element.style.backgroundColor = "#f5fffa"; // mintcream
+}
+
+function onResultFieldMouseOut(element) {
+  element.style.backgroundColor = "#ffffff"; // white
+}
